cbd4a7c80f36544d267a742bdaec9655dadacd35
HoffsMH <matthecker@pm.me>
2020-08-17 10:38:27 -0400

    ok

---
+++ chroot.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+source .envrc
+
+# https://wiki.manjaro.org/Configure_Graphics_Cards
+mhwd -a pci nonfree 0300
+
+echo -e "
+KEYMAP=us
+FONT=ter-116n" > /etc/vconsole.conf
+
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US ISO-8859-1" >> /etc/locale.gen
+locale-gen
+
+
+ntpd -qg
+timedatectl set-timezone "$(curl --fail https://ipapi.co/timezone)"
+hwclock -w
+
+echo $MACHINE_NAME > /etc/hostname
+systemctl enable dhcpcd
+systemctl enable NetworkManager
+
+# set root passwd
+echo -e "$ROOT_PASSWORD\n$ROOT_PASSWORD" | passwd
+useradd $USER_NAME sys network,power,video,storage,lp,input,audio,wheel
+echo -e "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USER_NAME
+
+mkinitcpio -p $LINUX_KERNEL
+
+genfstab -U / >> /mnt/etc/fstab
+
+# install refind
+refind-install
+
+
--- run.sh
+++ run.sh
@@ -136,23 +136,24 @@ basestrap /mnt $LINUX_KERNEL \
   mobile-broadband-provider-info \
   modemmanager \
   openresolv \
-  openssh
-  # samba \
-  # usb_modeswitch \
-  # alsa-firmware \
-  # alsa-utils \
-  # ffmpeg \
-  # gst-libav \
-  # gst-plugins-base \
-  # gst-plugins-good \
-  # gst-plugins-ugly \
-  # libdvdcss \
-  # manjaro-pulse \
-  # pulseaudio-bluetooth \
-  # pulseaudio-ctl \
-  # pulseaudio-zeroconf \
-  # android-tools \
-  # android-udev \
+  openssh \
+  arch-install-scripts \
+  samba \
+  usb_modeswitch \
+  alsa-firmware \
+  alsa-utils \
+  ffmpeg \
+  gst-libav \
+  gst-plugins-base \
+  gst-plugins-good \
+  gst-plugins-ugly \
+  libdvdcss \
+  manjaro-pulse \
+  pulseaudio-bluetooth \
+  pulseaudio-ctl \
+  pulseaudio-zeroconf \
+  android-tools \
+  android-udev
   # gvfs \
   # gvfs-afc \
   # gvfs-gphoto2 \
@@ -195,41 +196,10 @@ basestrap /mnt $LINUX_KERNEL \
   # vim \
   # links
 
-manjaro-chroot /mnt
+cp .envrc /mnt/
+cp chroot.sh /mnt/
 
-# https://wiki.manjaro.org/Configure_Graphics_Cards
-mhwd -a pci nonfree 0300
-
-echo -e "
-KEYMAP=us
-FONT=ter-116n" > /etc/vconsole.conf
-
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-echo "en_US ISO-8859-1" >> /etc/locale.gen
-locale-gen
-
-
-ntpd -qg
-timedatectl set-timezone "$(curl --fail https://ipapi.co/timezone)"
-hwclock -w
-
-echo $MACHINE_NAME > /etc/hostname
-systemctl enable dhcpcd
-systemctl enable NetworkManager
-
-# set root passwd
-echo -e "$ROOT_PASSWORD\n$ROOT_PASSWORD" | passwd
-useradd $USER_NAME sys network,power,video,storage,lp,input,audio,wheel
-echo -e "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USER_NAME
-
-mkinitcpio -p $LINUX_KERNEL
-
-genfstab -U / >> /mnt/etc/fstab
-
-# install refind
-refind-install
-
+manjaro-chroot /mnt chroot.sh
 
 exit
 umount -R /mnt
